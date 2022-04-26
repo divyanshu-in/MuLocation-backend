266415b296ee077253749a9cc74085fd1ac4e275
divyanshu-in <cdivyanshu7@gmail.com>
2022-04-26 22:40:16 +0530

    add response for normal http get request on server.

--- src/main/kotlin/com/divyanshu_in/Application.kt
+++ src/main/kotlin/com/divyanshu_in/Application.kt
@@ -1,13 +1,29 @@
 package com.divyanshu_in
 
 import com.divyanshu_in.web_sockets.module
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
     embeddedServer(Netty, port = 8000) {
-
+        routing {
+            muLocationRouting()
+        }
         module()
 
     }.start(wait = true)
+}
+
+
+fun Route.muLocationRouting() {
+    route("") {
+
+        get("{id?}") {
+            call.respondText("Mulocation Backend Server Is Active!")
+        }
+
+    }
 }
